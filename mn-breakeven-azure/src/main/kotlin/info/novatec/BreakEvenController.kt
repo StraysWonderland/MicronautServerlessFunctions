d98705dab4769009fe971e79545d857f45ce0038
Cagri Tasci <cagri.c.tasci@outlook.de>
2021-05-21 13:31:47 +0200

    Update BreakEvenController.kt

--- mn-breakeven-azure/src/main/kotlin/info/novatec/BreakEvenController.kt
+++ mn-breakeven-azure/src/main/kotlin/info/novatec/BreakEvenController.kt
@@ -7,20 +7,17 @@ import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.Produces
 import kotlin.math.ceil
 
+@Controller("/breakeven")
 class BreakEvenController {
 
-    @Controller("/breakeven")
-    class PetController {
+    @Get
+    @Produces(APPLICATION_JSON)
+    fun returnSampleValue(): Int {
+        return 42
+    }
 
-        @Get
-        @Produces(APPLICATION_JSON)
-        fun returnSampleValue(): Int {
-            return 42
-        }
-
-        @Post(processes = [APPLICATION_JSON])
-        fun breakeven(price: Double, fixedCosts: Double, unitCosts: Double): Int {
-            return ceil(fixedCosts / (price - unitCosts)).toInt()
-        }
+    @Post(processes = [APPLICATION_JSON])
+    fun breakeven(price: Double, fixedCosts: Double, unitCosts: Double): Int {
+        return ceil(fixedCosts / (price - unitCosts)).toInt()
     }
 }
